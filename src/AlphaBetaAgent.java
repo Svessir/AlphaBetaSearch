cd0a5843eee551c3989a0b67797b007f96fe3245
Sverrir <sverrir1993@gmail.com>
2016-02-17 15:36:25 +0000

    First commit

---
+++ src/AlphaBetaAgent.java
@@ -0,0 +1,24 @@
+
+public class AlphaBetaAgent implements Agent {
+	
+	private String role; // the name of this agent's role (white or BLACK)
+	private int playclock; // this is how much time (in seconds) we have before nextAction needs to return a move
+	private boolean myTurn; // whether it is this agent's turn or not
+	private int width, height; // dimensions of the board
+	
+	@Override
+	public void init(String role, int width, int height, int playclock) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public String nextAction(int[] lastmove) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public void cleanup() {
+		// TODO Auto-generated method stub
+		
+	}
+}
